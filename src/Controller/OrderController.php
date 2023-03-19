0a710b8fca80f7fb413f9a8abaa4b14b6c2671b8
MagInventor <programistmag@gmail.com>
2023-03-19 04:41:35 +0300

    feat: add load data function

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -11,8 +11,33 @@ class OrderController extends AbstractController
     #[Route('/order', name: 'app_order')]
     public function index(): Response
     {
+        $data_csv = $this->loadData('../public/data/dataFeb-2-2017.csv');
+        $data_json = $this->loadData('../public/data/dataFeb-2-2017.json');
+        $data_ldif = $this->loadData('../public/data/dataFeb-2-2017.ldif');
+        echo '<pre>';
+        print_r($data_csv);
+        print_r($data_json);
+        print_r($data_ldif);
+        echo '</pre>';
+
         return $this->render('order/index.html.twig', [
             'controller_name' => 'OrderController',
         ]);
+    }
+
+    function loadData($filename)
+    {
+        $ext = file_exists($filename) ? strtolower(pathinfo($filename, PATHINFO_EXTENSION)) : '';
+
+        switch($ext) {
+            case 'csv':
+                return array_map('str_getcsv', file($filename));
+            case 'json':
+                return json_decode(file_get_contents($filename), true);
+            case 'ldif':
+                return file_get_contents($filename);
+            default:
+                return [];
+        }
     }
 }
